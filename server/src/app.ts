626d7e662be03f1b93261f2f151868147a29df09
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2023-12-01 19:23:14 -0300

    feat: implement routing system to the application

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import config from "config";
 
+// Routes
+import router from "./router";
+
 // application instantiation
 const app = express();
 
 // JSON middleware
 app.use(express.json());
+
+app.use("/", router);
 
 // application port
 const port = config.get<number>("port");
