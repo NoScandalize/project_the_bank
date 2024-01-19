a68e1783276262e03535e23ee7597e4c86c4fb7a
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2024-01-19 02:46:15 -0300

    fix: Resolve issues in the logging system
    - Resolve issues in the logging system
    - Add rule to .gitignore

--- server/config/logger.ts
+++ server/config/logger.ts
@@ -7,7 +7,7 @@ class Logger {
 
         const date: Date = new Date();
 
-        const formatedDate: string = `${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}:${date.getSeconds().toString().padStart(2, "0")} ${date.getDate().toString().padStart(2, "0")}-${date.getMonth()}-${date.getFullYear()}`
+        const formatedDate: string = `${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}:${date.getSeconds().toString().padStart(2, "0")} ${date.getDate().toString().padStart(2, "0")}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getFullYear()}`
 
         const log: string = `${formatedDate} [${"INFO"}]: ${message}\n`;
 
